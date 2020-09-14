dfac89bf74af4956030b5c2ade0a684685486e0f
SevereCloud <SevereCloud@users.noreply.github.com>
2020-09-14 10:09:17 +0000

    deploy: 92e50381d86af36d220411cbbda59a69f4b6d80d

---
+++ precache-manifest.3befd25a7100a56513a0438b04c6397f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a11f7afe04fd4f0b879a1d46ffab09a",
+    "url": "/task-vkminiapps/index.html"
+  },
+  {
+    "revision": "41983c96f9dfdf54f4d6",
+    "url": "/task-vkminiapps/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "41983c96f9dfdf54f4d6",
+    "url": "/task-vkminiapps/static/js/2.08ad001f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/task-vkminiapps/static/js/2.08ad001f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bb216ba61d00b0f0504",
+    "url": "/task-vkminiapps/static/js/main.4f44c7f6.chunk.js"
+  },
+  {
+    "revision": "8c2e3118c7bea41d16eb",
+    "url": "/task-vkminiapps/static/js/runtime-main.f82dead5.js"
+  }
+]);
